656197472549ffe8adc78f08f7211620242bca7e
fajar.farisya <fajar.farisya@pactindo.com>
2024-04-26 11:20:13 +0700

    fixing tailwind css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,19 @@ module.exports = {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: 'Poppins, sans-serif',
+    },
+    extend: {
+      colors: {
+        primary: '#4640DE',
+        secondary: '#F6F6FD',
+        grey: '#ABB3C4',
+        dark: '#121F3E',
+        page: '#F8F8FA',
+        success: '#2ED16C',
+      },
+    },
   },
   plugins: [],
 }
-
